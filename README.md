48f17cf0ad9968368f8c12c6a245ac4faa15cfa3
Ivo Georgiev <ivogeorg@gmail.com>
2024-05-28 22:23:03 +0000

    more robus obstacle avoidance
    still vulnerable to rare corner cases, for example starting forward
    very close to a wall, as it can see no obstacle forward so it creeps
    closer to the wall and eventually the wheel catches

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ### topics_quiz
+
+To have the robot "patrol" (i.e. move constantly while avoiding obstacles), there are three factors:
+1. The frequency of the messages to `/cmd_vel`.  
+2. The angle ranges for left, front, and right.
+3. The linear and angular speeds of the robot.
--- src/topics_quiz.cpp
+++ src/topics_quiz.cpp
@@ -1,6 +1,7 @@
 #include "geometry_msgs/msg/detail/twist__struct.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "rclcpp/executors.hpp"
+#include "rclcpp/logging.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/detail/laser_scan__struct.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
@@ -18,7 +19,7 @@ public:
     publisher_ =
         this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
     timer_ = this->create_wall_timer(
-        1s, std::bind(&BallAvoider::velocity_callback, this));
+        500ms, std::bind(&BallAvoider::velocity_callback, this));
     subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
         "scan", 10, std::bind(&BallAvoider::laser_scan_callback, this, _1));
   }
@@ -29,12 +30,15 @@ private:
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
   sensor_msgs::msg::LaserScan laser_scan_data_;
   geometry_msgs::msg::Twist vel_cmd_msg_;
-//   const int RIGHT_FROM = 0, RIGHT_TO = 179;
-//   const int LEFT_FROM = 540, LEFT_TO = 719;
-//   const int FRONT_FROM = 180, FRONT_TO = 539;
+
+  // angle ranges
   const int RIGHT_FROM = 0, RIGHT_TO = 89;
   const int LEFT_FROM = 630, LEFT_TO = 719;
-  const int FRONT_FROM = 270, FRONT_TO = 449;
+  const int FRONT_FROM = 220, FRONT_TO = 499; // wider by 50 degrees
+
+  const double VELOCITY_INCREMENT = 0.1;
+  const double ANGULAR_BASE = 0.5;
+  const double LINEAR_BASE = 0.8;
 
   // publisher
   void velocity_callback();
@@ -49,13 +53,12 @@ private:
 
 // publisher
 void BallAvoider::velocity_callback() {
-  // TODO: avoid ball
   /*
-  The logic that your program has to follow is the next one:
+  Applroximate logic:
 
   1. If the laser reading in front of the robot is higher than one meter (there
-  is no obstacle closer than one meter in front of the robot), the robot will
-  move forward.
+     is no obstacle closer than one meter in front of the robot), the robot will
+     move forward.
   2. If the laser reading in front of the robot is lower than one meter
      (there is an obstacle closer than one meter in front of the robot), the
   robot will turn left.
@@ -65,22 +68,30 @@ void BallAvoider::velocity_callback() {
   4. If the laser reading on the left side
      of the robot is lower than one meter (there is an obstacle closer than one
      meter on the left side of the robot), the robot will turn right.
+
+  A robust algorithm would be a lot more complex. This one is fun :)
   */
 
   if (!obstacle_in_range(FRONT_FROM, FRONT_TO, 1.0)) {
     vel_cmd_msg_.linear.x = 0.8;
     vel_cmd_msg_.angular.z = 0.0;
   } else {
-    // vel_cmd_msg_.linear.x = 0.1;
+    // stop forward motion
     vel_cmd_msg_.linear.x = 0.0;
+
     bool obstacle_left = obstacle_in_range(LEFT_FROM, LEFT_TO, 1.0);
     bool obstacle_right = obstacle_in_range(RIGHT_FROM, RIGHT_TO, 1.0);
-    if (obstacle_left && ! obstacle_right) {
-      vel_cmd_msg_.angular.z = -0.8;
-    } else if (!obstacle_left && obstacle_right) {
-      vel_cmd_msg_.angular.z = 0.8;
+    bool turning_left = vel_cmd_msg_.angular.z > 0.0;
+    bool turning_right = vel_cmd_msg_.angular.z < 0.0;
+
+    // this one is robus but for a corner case of going straight very near a wall
+    // can correct with some logic to anticipate obstacles in front
+    if (obstacle_left && turning_right) {
+      vel_cmd_msg_.angular.z += - VELOCITY_INCREMENT;
+    } else if (obstacle_right && turning_left) {
+      vel_cmd_msg_.angular.z += VELOCITY_INCREMENT;
     } else {
-      vel_cmd_msg_.angular.z = 0.8;
+      vel_cmd_msg_.angular.z = 0.5;
     }
   }
 
@@ -91,12 +102,10 @@ void BallAvoider::velocity_callback() {
 void BallAvoider::laser_scan_callback(
     const sensor_msgs::msg::LaserScan::SharedPtr msg) {
   laser_scan_data_ = *msg;
-  //   RCLCPP_INFO(this->get_logger(), "Laser scanner has %ld scan beams",
-  //               laser_scan_data_.ranges.size());
   // ranges[0] is to the right
   // ranges[719] is to the left
-  RCLCPP_INFO(this->get_logger(), "Distance to the left is %f",
-              laser_scan_data_.ranges[719]);
+  RCLCPP_DEBUG(this->get_logger(), "Distance to the left is %f",
+               laser_scan_data_.ranges[719]);
 }
 
 bool BallAvoider::obstacle_in_range(int from, int to, double dist) {
